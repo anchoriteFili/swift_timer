6af42b67b1eeb35dc684d9ddb447145dbbfafe00
anchoriteFili <feiliyaailiya1@ilcoud.com>
2018-05-30 14:25:25 +0800

    提交一下啊

--- swift_timer/swift_timer/ViewController.swift
+++ swift_timer/swift_timer/ViewController.swift
@@ -13,6 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        DispatchTimer(timeInterval: 1.0, repeatCount: 60) { (timer, count) in
+            print("剩余的次数： \(count)")
+        }
+        
+        
+        
+        
+        
+    }
+    
+    /// GCD定时器倒计时⏳
+    ///   - timeInterval: 循环间隔时间
+    ///   - repeatCount: 重复次数
+    ///   - handler: 循环事件, 闭包参数： 1. timer， 2. 剩余执行次数
+    public func DispatchTimer(timeInterval: Double, repeatCount:Int, handler:@escaping (DispatchSourceTimer?, Int)->())
+    {
+        if repeatCount <= 0 {
+            return
+        }
+        let timer = DispatchSource.makeTimerSource(flags: [], queue: DispatchQueue.main)
+        var count = repeatCount
+        timer.schedule(wallDeadline: .now(), repeating: timeInterval)
+        timer.setEventHandler(handler: {
+            count -= 1
+            DispatchQueue.main.async {
+                handler(timer, count)
+            }
+            if count == 0 {
+                timer.cancel()
+            }
+        })
+        timer.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
